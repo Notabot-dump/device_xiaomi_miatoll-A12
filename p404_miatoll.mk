a5c3cb06a0b5f3bd10fa057dcd0dcf1c2a57c6cd
Arjun-Ingole <arjuningole38@gmail.com>
2021-11-10 15:59:33 +0600

    miatoll: Initial Project 404 bringup - Add a script to clone Hals
    Signed-off-by: Arjun-Ingole <arjuningole38@gmail.com>

--- p404_miatoll.mk
+++ p404_miatoll.mk
@@ -8,16 +8,23 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project 404 stuff.
+$(call inherit-product, vendor/404/configs/common.mk)
 
 # Inherit from miatoll device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := p404_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := SM6250
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Google Apps
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Hals
+git clone https://github.com/Arjun-Ingole/android_hardware_qcom_display hardware/qcom/display
+git clone https://github.com/Arjun-Ingole/android_hardware_qcom_media hardware/qcom/media
+git clone https://github.com/Arjun-Ingole/android_hardware_qcom_audio vendor/qcom/opensource/audio-hal/primary-hal
